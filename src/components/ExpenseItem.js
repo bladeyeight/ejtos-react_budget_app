1b8dfa32f5fc4b5fafc08294b04b8a4fb271105a
bladeyeight <samueljperry1991@gmail.com>
2024-02-01 13:57:39 -0500

    completed project

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,11 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
-import { BsPlusCircle } from "react-icons/bs";
+import { BsPlus } from "react-icons/bs";
+import { FiMinus } from "react-icons/fi";
+
+
+
 
 
 const ExpenseItem = (props) => {
@@ -27,11 +31,25 @@ const ExpenseItem = (props) => {
 
     }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: -10,
+        };
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+
+    }
+
     return (
         <tr>
         <td>{props.name}</td>
         <td>£{props.cost}</td>
-        <td><BsPlusCircle style={{ color: 'white', backgroundColor: "green", borderRadius: "50%" }} onClick={event=> increaseAllocation(props.name)}>+</BsPlusCircle></td>
+        <td><BsPlus style={{ color: 'white', backgroundColor: "green", borderRadius: "55%", width: '20', height: '20' }} onClick={event=> increaseAllocation(props.name)}>+</BsPlus></td>
+        <td><FiMinus style={{borderRadius: "60%", backgroundColor: '#CC0202', color: 'white', width: '20', height: '20'}} onClick={event=> decreaseAllocation(props.name)}>+</FiMinus></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
